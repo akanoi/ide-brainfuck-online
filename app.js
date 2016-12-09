2b018df015cf62a202eedaa5891c0a2db2e91f35
akanoi <aleh.i.kanoika@gmail.com>
2016-12-09 22:28:28 +0300

    Initialize project.

---
+++ app.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var config = require('./config');
+var mongoose = require('./libs/mongoose');
+var http = require('http');
+var log = require('./libs/log')(module);
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.engine('ejs', require('ejs-locals'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var session = require('express-session');
+
+var MongoStore = require('connect-mongo')(session);
+app.use(session({
+    secret: config.get('session:secret'),
+    key: config.get('session:key'),
+    cookie: config.get('session:cookie'),
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+
+app.use(require('./middleware/sendHttpError'));
+app.use(require('./middleware/loadUser'));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found :C');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+http.createServer(app).listen(config.get('port'), function(){
+    log.info('Express server listening on port ' + config.get('port'));
+});
+
+// module.exports = app;
---
+++ models/file.js
@@ -0,0 +1,92 @@
+var async = require('async');
+var util = require('util');
+
+var mongoose = require('../libs/mongoose'),
+    Schema = mongoose.Schema;
+
+var schema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    fileName: {
+        type: String,
+        required: true
+    },
+    fileText: {
+        type: String
+    }
+});
+
+schema.statics.allUsersFiles = function(username, callback) {
+    var File = this;
+    async.waterfall([
+        function(callback) {
+            File.find({username: username}, callback);
+        },
+        function(recordsByUser, callback) {
+            if (recordsByUser) {
+                callback(recordsByUser);
+            } else {
+                callback("No Text");
+            }
+        }
+    ], callback);
+};
+
+schema.statics.addFile = function(username, fileName, fileText, callback) {
+    var File = this;
+    async.waterfall([
+        function(callback) {
+            File.findOne({id: username+fileName}, callback);
+        },
+        function(file, callback) {
+            if (file) {
+                File.remove(file, callback);
+            }
+            var newFile = new File({id: username+fileName, username: username, fileName: fileName, fileText: fileText});
+            newFile.save(callback);
+        }
+    ], callback);
+};
+
+schema.statics.deleteFile = function(username, fileName, callback) {
+    var File = this;
+    async.waterfall([
+        function(callback) {
+            File.findOne({id: username+fileName}, callback);
+        },
+        function(file, callback) {
+            if (file)
+                File.remove(file, callback);
+        }
+    ], callback);
+};
+
+schema.statics.renameFile = function(username, newFileName, oldFileName, callback){
+    var File = this;
+    async.waterfall([
+        function(callback) {
+            File.findOne({id:username+newFileName}, callback);
+        },
+        function(file, callback) {
+            if (file) {
+                callback("error");
+                return;
+            }
+            File.findOne({id:username+oldFileName}, callback);
+        },
+        function(file, callback) {
+            var newFile = new File({id:username+newFileName, username: username, fileName: newFileName, fileText: file.fileText});
+            File.remove(file, callback);
+            newFile.save(callback);
+        }
+    ], callback);
+};
+
+exports.File = mongoose.model('File', schema);
